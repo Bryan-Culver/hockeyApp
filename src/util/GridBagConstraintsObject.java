aebccfaf81d80580d24c333ee63f6e2636df2932
BoringBryan <culver.bryan.t@gmail.com>
2023-04-07 13:20:16 -0400

    created a new class to manage GridBagConstraints, and created a JUnit test class for it. All tests are written and are failing as appropriate.

---
+++ src/util/GridBagConstraintsObject.java
@@ -0,0 +1,80 @@
+/**
+ * 
+ */
+package util;
+
+import java.awt.GridBagConstraints;
+
+/**This class is an object form of 
+ * @author Bryan Culver
+ * @version 7 April 2023
+ */
+public class GridBagConstraintsObject extends GridBagConstraints{
+	private static final long serialVersionUID = 1L;
+	
+	public GridBagConstraintsObject() {
+		super();
+	}
+	
+	public GridBagConstraintsObject gridx(int x) {
+		this.gridx = x;
+		return this;
+	}
+	
+	public GridBagConstraintsObject gridy(int y) {
+		this.gridy = y;
+		return this;
+	}
+
+	public void insets(int testint) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void insets(int i, int j, int k, int l) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void anchor(int firstLineEnd) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void fill(int both) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void gridheight(int i) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void gridwidth(int i) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void ipadx(int testint) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void ipady(int testint) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void weightx(double d) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void weighty(double d) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	
+}
