445f24fe0ab18725bfcdfe3b9636d1fd69c07f00
BoringBryan <culver.bryan.t@gmail.com>
2023-04-14 14:57:39 -0400

    Customized the appearance of the window, and established a custom 'JTextArea' class that uses my default settings. Also, removed method 'Close' since it did nothing and I changed the 'Done' button to 'Save'. Finished refactoring within scope.

--- src/view/TeamBuildingView.java
+++ src/view/TeamBuildingView.java
@@ -1,17 +1,15 @@
 package view;
 
-import java.awt.Dimension;
 import java.awt.GridBagConstraints;
 import java.awt.GridBagLayout;
+import java.awt.event.ActionListener;
 
 import javax.swing.JButton;
 import javax.swing.JFrame;
-//import javax.swing.JTextField;
 
 import util.GridBagConstraintsObject;
 
 import javax.swing.JPanel;
-import javax.swing.JTextField;
 import javax.swing.JLabel;
 
 public class TeamBuildingView extends JFrame{
@@ -19,10 +17,8 @@ public class TeamBuildingView extends JFrame{
 	 * 
 	 */
 	private static final long serialVersionUID = 1L;
-	private JButton doneButton;
-	//private JTextField teamTitleTF;
-	private JLabel teamTitleLabel;
 	private JPanel panel;
+	private ActionListener saveListener; // TODO Implement listener class in main package. 
 	
 	public TeamBuildingView(String title) {
 		super(title);
@@ -30,9 +26,18 @@ public class TeamBuildingView extends JFrame{
 		GridBagLayout gbl = new GridBagLayout();
 		panel = new JPanel(gbl);
 		
-		teamTitleLabel = new JLabel("Team Title");
-		doneButton = new JButton("DONE");
+		JLabel teamTitleLabel = new JLabel("Team Title");
+		JButton saveButton = new JButton("Save");
+		JButton editButton = new JButton("Edit");
+		int cupWins = 0; // TODO implement a get method for this. 
+		int standingLastYear = 12; // TODO implement a get method for this. 
 		
+		// Implemented a HockeyViewTextArea class to avoid repeating changes to the JTextAreas. 
+		HockeyViewTextArea cupWinsTA = new HockeyViewTextArea(Integer.toString(cupWins),this.getBackground());
+		HockeyViewTextArea standingTA = new HockeyViewTextArea(Integer.toString(standingLastYear), this.getBackground());
+		HockeyViewTextArea userNameTA = new HockeyViewTextArea("username", this.getBackground());
+		HockeyViewTextArea locationTA = new HockeyViewTextArea("City, State", this.getBackground());
+		HockeyViewTextArea teamNameTA = new HockeyViewTextArea("Team Name", this.getBackground());
 		
 		GridBagConstraintsObject gbco = new GridBagConstraintsObject(); 
 		gbco.insets(15).anchor(GridBagConstraints.LINE_START).fill(GridBagConstraints.VERTICAL).ipadx(2).ipady(2);
@@ -43,20 +48,21 @@ public class TeamBuildingView extends JFrame{
 		panel.add(new JLabel("Stanley Cup Wins"), gbco.gridy(3).gridx(0));
 		panel.add(new JLabel("Last Year's standing"), gbco.gridy(4).gridx(0));
 		
-		panel.add(new JTextField("username"),gbco.gridx(1).gridy(0).fill(GridBagConstraints.HORIZONTAL));
-		panel.add(new JTextField("City, State"), gbco.gridx(1).gridy(1));
-		panel.add(new JTextField("Team Name"), gbco.gridx(1).gridy(2));
-		panel.add(new JTextField("0"), gbco.gridx(1).gridy(3));
-		panel.add(new JTextField("12"), gbco.gridx(1).gridy(4).fill(GridBagConstraints.HORIZONTAL));
+		panel.add(userNameTA,gbco.gridx(1).gridy(0).fill(GridBagConstraints.HORIZONTAL).weightx(0.6));
+		panel.add(locationTA, gbco.gridx(1).gridy(1));
+		panel.add(teamNameTA, gbco.gridx(1).gridy(2));
+		panel.add(cupWinsTA, gbco.gridx(1).gridy(3));
+		panel.add(standingTA, gbco.gridx(1).gridy(4).fill(GridBagConstraints.HORIZONTAL));
 		
-		panel.add(doneButton, gbco.gridy(5).gridx(4).anchor(GridBagConstraints.LAST_LINE_END));
+		panel.add(editButton, gbco.gridx(0).gridy(5).anchor(GridBagConstraints.LAST_LINE_START).fill(0));
+		panel.add(saveButton, gbco.gridy(5).gridx(4).anchor(GridBagConstraints.LAST_LINE_END).weightx(0));
+		
+		saveButton.addActionListener(saveListener); // TODO Implement listener class in main package
+		
 		this.getContentPane().add(panel);
 		this.setLocationRelativeTo(null);
 		this.setDefaultCloseOperation(EXIT_ON_CLOSE);
 		
-		this.setSize(new Dimension(500,500));
-	}
-	
-	public void Close() {
+		this.pack();
 	}
 }
