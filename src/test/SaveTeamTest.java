84180b48219eff33a7e8248f53bf10e8bf48ca77
BoringBryan <culver.bryan.t@gmail.com>
2023-09-22 23:31:41 -0400

    Added initial file to test the save function, and added a Team object, to provide something to save

---
+++ src/test/SaveTeamTest.java
@@ -0,0 +1,24 @@
+/**
+ * 
+ */
+package test;
+
+import org.junit.Test;
+import org.junit.jupiter.api.BeforeAll;
+
+/**
+ * @author Bryan Culver
+ *
+ */
+class SaveTeamTest {
+
+	@BeforeAll
+	void TeamSetup() {
+		
+	}
+	
+	@Test
+	void TeamSaveTest(){
+		
+	}
+}
